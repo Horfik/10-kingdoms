8373cda8bd05c250a0ee55800cd0b08d6d932e85
Horfik <demchuk1997@bk.ru>
2023-07-01 10:05:37 +0300

    delete master stage (bad worked)

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -3,20 +3,11 @@ import {useForm } from '@inertiajs/vue3';
 
 export default createStore({
     state: () => ({
-        master: localStorage.getItem('master')
+
     }),
     mutations: {
-        setMaster(stage, user) {
-            console.log(stage, user)
-            if(user.role === 'master')
-            {
-                stage.master = true
-            }
-        },
         logout(stage) {
             const form = useForm({});
-            localStorage.removeItem('master')
-            stage.master = null;
             form.post(route('logout'));
         },
     }
